947b8c5c6ad68fa145bc76a528d40b01a7247685
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-25 22:44:24 +0630

    Blog Model を作成し、外部キーを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_160058) do
+ActiveRecord::Schema.define(version: 2021_04_25_161226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.text "content"
+    t.text "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_04_25_160058) do
     t.text "image"
   end
 
+  add_foreign_key "blogs", "users"
 end
