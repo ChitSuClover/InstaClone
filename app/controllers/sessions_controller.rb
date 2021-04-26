23b569651ce4e2fff9fa9cb15156c63e4eaab4d0
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-26 21:51:22 +0630

    feat:Session destroy action を作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,15 @@ class SessionsController < ApplicationController
     user=User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id]=user.id
-      redirect_to user_path(@user.id)
+      redirect_to user_path(user.id)
     else
       flash.now[:danger]="Login Fail"
       render :new
     end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice]="Logged out"
+    redirect_to new_session_path
   end
 end
