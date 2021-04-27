83a47a7869b1f88a9503b0a15f548d8226b2144c
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-27 20:53:14 +0630

    feat: Blog confirm アクションを設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,14 +7,21 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
   def create
-    
+
   end
   def edit
   end
   def show
   end
+  def confirm
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
   private
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+  def blog_params
+    params.require(:blog).permit(:content, :image, :image_cache)
   end
 end
