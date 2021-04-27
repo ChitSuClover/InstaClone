cad1c2b24bdac489727c776150a4891c3b0152d3
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-27 20:41:52 +0630

    feat:confirm フォームに追記

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,20 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :show, :update, :destroy]
   def index
     @blogs = Blog.all
   end
   def new
     @blog = Blog.new
+  end
+  def create
+    
+  end
+  def edit
+  end
+  def show
+  end
+  private
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
