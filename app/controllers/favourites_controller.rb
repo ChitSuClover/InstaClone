f129dc25975fd2ea5dc34e39fb5fb2c203e653f0
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-29 20:37:28 +0630

    feat:Favouriteのcreate アクション＆destroyアクションを作成

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,10 @@
 class FavouritesController < ApplicationController
+  def create
+    favourite = current_user.favourites.create(blog_id: params[:blog_id])
+    redirect_to blogs_path, notice: "Liked"
+  end
+  def destroy
+    current_user.favourites.find_by(id: params[:id]).destroy
+    redirect_to blogs_path, notice: "Unliked"
+  end
 end
